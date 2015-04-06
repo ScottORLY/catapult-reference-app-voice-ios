23438d197f8fc000e7add2bf5953aea72d95e012
Fabio Rodella <frodella@daitangroup.com>
2015-04-06 07:46:44 -0300

    Added config file and build phase that checks if the default values were changed.

---
+++ Bandwidth Voice Ref App/Classes/Config.swift
@@ -0,0 +1,20 @@
+//
+//  Config.swift
+//  Bandwidth Voice Ref App
+//
+//  Created by Fabio Rodella on 4/6/15.
+//  Copyright (c) 2015 Bandwidth. All rights reserved.
+//
+
+import Foundation
+
+/**
+    Structure containing configuration values
+*/
+struct Config {
+    
+    /**
+        The base server URL. Must be replaced with your own URL, otherwise the project won't build.
+    */
+    let serverUrl = "<replace me>"
+}
